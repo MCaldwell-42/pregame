4220ec210c219b22233ef364f9e77fc567e2b9d7
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-10-25 11:28:34 -0500

    google api workiiiing wooooo

--- pregameApp/views/events/events_list.py
+++ pregameApp/views/events/events_list.py
@@ -63,9 +63,9 @@ def event_list(request):
         form_data = request.POST
         with sqlite3.connect(Connection.db_path) as conn:
             gmaps = googlemaps.Client(key=Maps)
-            geocode_result = gmaps.geocode(form_data['address'])
-            event_lat = geocode_result.result.geometry.location.lat
-            event_long = geocode_result.result.geometry.location.lng
+            geocode_result = gmaps.geocode(form_data['address'])[0]
+            event_lat = geocode_result['geometry']['location']['lat']
+            event_long = geocode_result['geometry']['location']['lng']
             current_user = request.user
 
             db_cursor = conn.cursor()
@@ -82,4 +82,4 @@ def event_list(request):
                 form_data['description'], form_data['date'],
                 form_data['time'], form_data['img_url'], event_lat, event_long, current_user.id ))
 
-        return redirect(reverse('pregameApp:employees'))
+        return redirect(reverse('pregameApp:events'))
