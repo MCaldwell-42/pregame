fe156c336e7a32e4629915da3dfad4dca289653a
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-10-29 10:41:46 -0500

    login working

--- pregameApp/views/__init__.py
+++ pregameApp/views/__init__.py
@@ -1,5 +1,6 @@
 from .home import home
 from .auth.logout import logout_user
+from .auth.login import login_user
 from .auth.register import register_user
 from .events.events_list import event_list
 from .events.events_form import event_form
